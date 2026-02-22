c269525b1302ea742cc1d1e7bab1b35df08ae5bb
Fernando <fdplopez@gmail.com>
2026-02-22 21:07:36 +0100

    refactor(api): use dependency injection for book repo

--- src/gestor_biblioteca_casa/entrypoints/api/routers/books.py
+++ src/gestor_biblioteca_casa/entrypoints/api/routers/books.py
@@ -1,13 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
 from gestor_biblioteca_casa.services.book_service import create_book, list_books
 from gestor_biblioteca_casa.bootstrap import get_book_repo
+from gestor_biblioteca_casa.ports.book_contract import BookRepo
 
 router = APIRouter(prefix='/books', tags=['Books'])
 
-repo = get_book_repo()
-
 @router.post('/')
-def new_book(title: str, author: str):
+def new_book(title: str, author: str, repo: BookRepo = Depends(get_book_repo)):
     new_id = create_book(repo, title, author)
 
     return {'id': new_id}
@@ -16,5 +16,6 @@ def new_book(title: str, author: str):
 def list_all_books(
     size: int=20,
     page: int=1,
+    repo: BookRepo = Depends(get_book_repo),
 ):
     return list_books(repo, size, page)
